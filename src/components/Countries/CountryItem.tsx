3acc14ec92d5cbbb0cee679dfe0671e24736ff86
12BSG12 <carlj8335@gmail.com>
2022-10-09 18:36:59 +0500

    added lezy-load in CountryItem

--- src/components/Countries/CountryItem.tsx
+++ src/components/Countries/CountryItem.tsx
@@ -2,34 +2,44 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { ICountry } from '../../model/countriesAPIType';
 import { Cart, CartImg, CartInfo } from './styles';
+import { useInView } from 'react-intersection-observer';
+import { Skeleton } from './Skeleton';
 
 export const CountryItem: FC<ICountry> = ({ flags, name, population, region, capital }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
   return (
-    <Link to={'country_info/' + name}>
-      <Cart>
-        <CartImg>
-          <img src={flags.png} alt="" />
-        </CartImg>
-        <CartInfo>
-          <h2>{name}</h2>
-          {population !== 0 && (
+    <Link to={'country_info/' + name} ref={ref}>
+      {inView ? (
+        <Cart>
+          <CartImg>
+            <img src={flags.png} alt="" />
+          </CartImg>
+          <CartInfo>
+            <h2>{name}</h2>
+            {population !== 0 && (
+              <div>
+                <span>Population:</span>
+                <p>{population}</p>
+              </div>
+            )}
             <div>
-              <span>Population:</span>
-              <p>{population}</p>
+              <span>Region:</span>
+              <p>{region}</p>
             </div>
-          )}
-          <div>
-            <span>Region:</span>
-            <p>{region}</p>
-          </div>
-          {capital && (
-            <div>
-              <span>Capital:</span>
-              <p>{capital}</p>
-            </div>
-          )}
-        </CartInfo>
-      </Cart>
+            {capital && (
+              <div>
+                <span>Capital:</span>
+                <p>{capital}</p>
+              </div>
+            )}
+          </CartInfo>
+        </Cart>
+      ) : (
+        <Skeleton />
+      )}
     </Link>
   );
 };
